694eaef21e69c65dcf69805e72ef4f39fb213c24
ulovebin <ulovewb@163.com>
2016-06-21 13:01:06 +0800

    Initial Commit

---
+++ storyBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  storyBoard
+//
+//  Created by tuhui－03 on 16/6/21.
+//  Copyright © 2016年 Qmap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
